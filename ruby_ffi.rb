17be3cf6909f7eebacba17576e20a7f7694bc78c
Guilherme Moreira <guilhermerx7@gmail.com>
2016-10-30 23:39:13 -0200

    Use typedef to give more meaningful names

--- ruby_ffi.rb
+++ ruby_ffi.rb
@@ -5,10 +5,13 @@ module ClSphere
   extend FFI::Library
   ffi_lib 'build/libcl_sphere.so'
 
-  attach_function :platforms_size, :get_platforms_size, [], :uint32
-  attach_function :platform, :get_platform, [:uint32], :pointer
-  attach_function :platform_name, :get_platform_name, [:pointer], :strptr
-  attach_function :devices_size, :get_devices_size, [:pointer], :uint32
+  typedef :pointer, :cl_platform_id
+  typedef :uint32, :cl_uint
+
+  attach_function :platforms_size, :get_platforms_size, [], :cl_uint
+  attach_function :platform, :get_platform, [:cl_uint], :cl_platform_id
+  attach_function :platform_name, :get_platform_name, [:cl_platform_id], :string
+  attach_function :devices_size, :get_devices_size, [:cl_platform_id], :cl_uint
 end
 
 platforms_size = ClSphere.platforms_size
@@ -17,7 +20,7 @@ puts "Listing #{platforms_size} platforms..."
 
 (0...platforms_size).each do |i|
   id = ClSphere.platform(i)
-  name, ptr = ClSphere.platform_name(id)
+  name = ClSphere.platform_name(id)
   puts name
 
   puts "Devices: #{ClSphere.devices_size(id)}"
